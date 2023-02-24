2076e2c9f738eb405c4c150162cca0da73cdbea1
Rahul Garg <rahulgargofficial@gmail.com>
2023-02-24 19:22:02 +0530

    q2

--- Q2.py
+++ Q2.py
@@ -5,17 +5,33 @@ import time
 
 random.seed(time.time())
 
-N = 100000
-
 
 def f(x):
     return x * x * np.cos(x)
 
 
 # monte carlo integration
-x = np.random.uniform(0, 1, N)
-x = sorted(x) * np.pi - np.pi / 2
-y = f(x).astype(np.float64)
+def monte_carlo(f, N):
+    x = np.random.uniform(0, 1, N)
 
-sum = (np.sum(y) / N) * np.pi
-print(sum)
+    x = np.array(sorted(x)) * np.pi - np.pi / 2
+
+    y = f(x).astype(np.float64)
+
+    return (np.sum(y) / N) * np.pi
+
+
+# a
+print(monte_carlo(f, 100000))
+
+# b
+# x = [1, 1000, 2000, ... , 100000]
+x = np.arange(1000, 100000 + 1, 1000)
+x = [1] + list(x)
+x = np.array(x)
+print(x)
+
+# plot
+y = np.array([monte_carlo(f, i) for i in x])
+plt.plot(x, y)
+plt.show()
